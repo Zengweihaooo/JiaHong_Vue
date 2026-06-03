53bac273002f36536d5df4e22e633c610eb68dd9
Zeng Weihao <Zengweihaooo@gmail.com>
2026-06-03 21:42:21 +0800

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
+const githubRepositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const base =
+  process.env.GITHUB_ACTIONS && githubRepositoryName
+    ? `/${githubRepositoryName}/`
+    : "/";
+
 export default defineConfig({
+  base,
   plugins: [vue()],
   resolve: {
     alias: {
